1fd19eb3ec8d6facf6e1e4e7a3a908b011c73834
FarhadF <farhad.farahi@gmail.com>
2018-05-28 15:36:30 +0430

    add stacktraces to service.go errs for users service

--- users/service.go
+++ users/service.go
@@ -38,25 +38,25 @@ func (s usersService) NewUser(ctx context.Context, user User) (string, error) {
 	rows, err := s.db.Query("select * from users where email='" + user.Email + "'")
 	defer rows.Close()
 	if err != nil {
-		return "", err
+		return "", errors.WithStack(err)
 	}
 	if !rows.Next() {
 		conn, err := grpc.Dial(":8085", grpc.WithInsecure(), grpc.WithTimeout(1*time.Second))
 		if err != nil {
-			return "", err
+			return "", errors.WithStack(err)
 		}
 		defer conn.Close()
 		vaultService := vaultClient.New(conn)
 		hash, err := vaultClient.Hash(ctx, vaultService, user.Password)
 		if err != nil {
-			return "", err
+			return "", errors.WithStack(err)
 		}
 		var id string
 		user.Role = "user"
 		err = s.db.QueryRow("insert into users (name, lastname, email, password, userrole) values($1,$2,$3,$4,$5) returning id",
 			user.Name, user.LastName, user.Email, hash, user.Role).Scan(&id)
 		if err != nil {
-			return "", err
+			return "", errors.WithStack(err)
 		}
 		//return strconv.FormatInt(id, 10), nil
 		return id, nil
@@ -73,7 +73,7 @@ func (s usersService) GetUserByEmail(ctx context.Context, email string) (User, e
 	err := s.db.QueryRow("select * from users where email='"+email+"'").Scan(&user.Id, &user.Name, &user.LastName,
 		&user.Email, &user.Password, &user.Role, &user.CreatedOn, &user.UpdatedOn, &user.LastLogin)
 	if err != nil {
-		return user, err
+		return user, errors.WithStack(err)
 	}
 	return user, nil
 }
@@ -83,28 +83,28 @@ func (s usersService) ChangePassword(ctx context.Context, email string, currentP
 	var currentPasswordHash string
 	err := s.db.QueryRow("select password from users where email='" + email + "'").Scan(&currentPasswordHash)
 	if err != nil {
-		return false, err
+		return false, errors.WithStack(err)
 	}
 	conn, err := grpc.Dial(":8085", grpc.WithInsecure(), grpc.WithTimeout(1*time.Second))
 	if err != nil {
-		return false, err
+		return false, errors.WithStack(err)
 	}
 	defer conn.Close()
 	vaultService := vaultClient.New(conn)
 	valid, err := vaultClient.Validate(ctx, vaultService, currentPassword, currentPasswordHash)
 	if err != nil {
-		return false, err
+		return false, errors.WithStack(err)
 	}
 	if valid != true {
-		return false, errors.New("wrong password")
+		return false, errors.WithStack(errors.New("wrong password"))
 	}
 	hash, err := vaultClient.Hash(ctx, vaultService, newPassword)
 	if err != nil {
-		return false, err
+		return false, errors.WithStack(err)
 	}
 	_, err = s.db.Exec("update users set password=$1 where email=$2", hash, email)
 	if err != nil {
-		return false, err
+		return false, errors.WithStack(err)
 	}
 	return true, nil
 }
@@ -112,27 +112,27 @@ func (s usersService) ChangePassword(ctx context.Context, email string, currentP
 func (s usersService) Login(ctx context.Context, email string, password string) (string, error) {
 	user, err := s.GetUserByEmail(ctx, email)
 	if err != nil {
-		return "", err
+		return "", errors.WithStack(err)
 	}
 	conn, err := grpc.Dial(":8085", grpc.WithInsecure(), grpc.WithTimeout(1*time.Second))
 	if err != nil {
-		return "", err
+		return "", errors.WithStack(err)
 	}
 	defer conn.Close()
 	vaultService := vaultClient.New(conn)
 	valid, err := vaultClient.Validate(ctx, vaultService, password, user.Password)
 	if valid != true {
-		return "", errors.New("wrong password")
+		return "", errors.WithStack(errors.New("wrong password"))
 	}
 	conn1, err := grpc.Dial(":8087", grpc.WithInsecure())
 	if err != nil {
-		return "", err
+		return "", errors.WithStack(err)
 	}
 	defer conn1.Close()
 	jwtService := jwtClient.NewGRPCClient(conn1)
 	token, err := jwtClient.GenerateToken(ctx, jwtService, email, user.Role)
 	if err != nil {
-		return "", err
+		return "", errors.WithStack(err)
 	}
 	return token, nil
 }
