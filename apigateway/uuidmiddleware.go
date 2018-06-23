12c56ae052c67bdb0e871594b13fdebde884f2ea
FarhadF <farhad.farahi@gmail.com>
2018-06-23 17:18:26 +0430

    inject ctx into grpc and pass through movies service

--- apigateway/uuidmiddleware.go
+++ apigateway/uuidmiddleware.go
@@ -11,10 +11,10 @@ import (
 //this will be used in register fuc in http_server.go
 func UUIDMiddleware(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		u2 := uuid.NewV4()
-		//if err != nil {
-		//	respondError(w, http.StatusInternalServerError, err)
-		//}
+		u2, err := uuid.NewV4()
+		if err != nil {
+			respondError(w, http.StatusInternalServerError, err)
+		}
 		ctx := r.Context()
 		ctx = context.WithValue(ctx, "correlationid", u2)
 		r = r.WithContext(ctx)
