98a21932317a935833a7c8d0057f8edcd674e139
FarhadF <farhad.farahi@gmail.com>
2018-06-27 10:35:05 +0430

    start span at uuid middleware

--- apigateway/uuidmiddleware.go
+++ apigateway/uuidmiddleware.go
@@ -3,6 +3,7 @@ package apigateway
 import (
 	"context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/opentracing/opentracing-go"
 	"github.com/satori/go.uuid"
 	"net/http"
 )
@@ -11,11 +12,19 @@ import (
 //this will be used in register fuc in http_server.go
 func UUIDMiddleware(next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		//get request context
+		ctx := r.Context()
+		//get the global tracer
+		tracer := opentracing.GlobalTracer()
+		//this is where we start our span for this operation, this will be the parent for this method
+		span := tracer.StartSpan("UUIDMiddleware")
+		defer span.Finish()
+		ctx = opentracing.ContextWithSpan(ctx, span)
 		u2 := uuid.NewV4()
 		/*if err != nil {
 			respondError(w, http.StatusInternalServerError, err)
 		}*/
-		ctx := r.Context()
+
 		ctx = context.WithValue(ctx, "correlationid", u2)
 		r = r.WithContext(ctx)
 		next(w, r, ps)
